ad36b633052d4bb8a1b3ee9de8da2a640375d28e
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-10-17 19:36:29 -0500

    Utterances

---
+++ _nuxt/static/1634517306/blog/tags/java/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634517306",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:"\u002Fblog\u002Ftags\u002Fjava\u002F1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d6200c21"}}}}(null));
