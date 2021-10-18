85945070f99a053c95d033fb167afd7b1261f1ba
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-10-17 21:51:09 -0500

    SEO Imgs

---
+++ _nuxt/static/1634525417/blog/2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634525417",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:"\u002Fblog\u002F2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7e74c8dd"}}}}(null));
