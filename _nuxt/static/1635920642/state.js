1c493875226e58d1eca6c2394d3ef3efaba79d0e
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-11-03 00:52:03 -0600

    Analytics events

--- _nuxt/static/1635920642/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635920642",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"58863803"}}}}(null,"\u002F"));
