9594081d31ad69749f413021646b47fb5a38fec9
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-11-20 23:18:44 -0600

    Post: Switch Syntax Highlighting Theme In @nuxt/content

---
+++ _nuxt/static/1637471903/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637471903",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f4d2dc34"}}}}(null,"\u002F"));
