708c5b8e28afecac663d6fe3cee1dc0b0837d760
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-12-12 18:28:46 -0600

    3 Linux/Kubuntu Posts

--- _nuxt/static/1638174086/blog/post/vue-router-scroll-behavior/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638174086",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:"\u002Fblog\u002Fpost\u002Fvue-router-scroll-behavior",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d8f0395"}}}}(null));
