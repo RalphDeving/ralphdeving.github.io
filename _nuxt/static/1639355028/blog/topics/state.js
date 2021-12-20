e3410259e9a98324458f78c0b90b27f3c2185bca
RalphDeving <39467623+RalphDeving@users.noreply.github.com>
2021-12-19 19:32:24 -0600

    Post: CPU and GPU monitoring on Kubuntu

--- _nuxt/static/1639355028/blog/topics/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639355028",layout:"default",error:a,state:{themeStore:{}},serverRendered:true,routePath:"\u002Fblog\u002Ftopics",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9a2fdf39"}}}}(null));
